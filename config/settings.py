0b7242e6e53392827038aef89e2967acfc625eb9
unvya <dnskoe7@gmail.com>
2026-08-10 03:54:53 +0900

    feat: 기본 권한 설정

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+    "rest_framework.permissions.IsAuthenticated",
+    ),
     "EXCEPTION_HANDLER": "common.exceptions.custom_exception_handler",
 }
 
