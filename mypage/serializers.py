6053bfc12928db2f7e18f897a304c50273f66386
jiwoolee211 <dorosi211@gmail.com>
2026-08-17 14:52:14 +0900

    fix: 프로필 수정 시 protectionEndDate 수정 가능하도록 변경

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -26,7 +26,7 @@ class RegionSerializer(serializers.Serializer):
 class ProfileSerializer(serializers.ModelSerializer):
     birthDate = serializers.DateField(source="birth_date", read_only=True)
     region = RegionSerializer(source="*", required=False)
-    protectionEndDate = serializers.DateField(source="protection_end_date", read_only=True)
+    protectionEndDate = serializers.DateField(source="protection_end_date", required=False)
     protectionType = serializers.ChoiceField(
         source="protection_type", choices=User.ProtectionType.choices, required=False,
     )
