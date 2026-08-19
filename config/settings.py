fcaa0ff07ba0926808686de6664c4aed0107da0c
unvya <dnskoe7@gmail.com>
2026-08-19 22:15:24 +0900

    feat: AWS 배포를 위한 PostgreSQL 데이터베이스 설정 추가 및 psycopg2-binary 패키지 추가

--- config/settings.py
+++ config/settings.py
@@ -129,12 +129,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("RDS_HOSTNAME"):   # aws
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("RDS_DB_NAME"),
+            "USER": os.getenv("RDS_USERNAME"),
+            "PASSWORD": os.getenv("RDS_PASSWORD"),
+            "HOST": os.getenv("RDS_HOSTNAME"),
+            "PORT": os.getenv("RDS_PORT", "5432"),
+        }
     }
-}
+else:   # 로컬
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
